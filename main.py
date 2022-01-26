6d1ee289bc202c7d105f569f340e4ab652268b2b
alinashaymardanova <98107093+alinashaymardanova@users.noreply.github.com>
2022-01-26 23:41:11 +0300

    Update and rename collatz.py to main.py

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-list1 = []
-
-def collatz(x):
-    list1.append(x)
-    while x != 1:
-        if x == 1:
-            list1.append(1)
-            continue
-        elif x % 2 == 0:
-            return x2(x)
-        else:
-            return x3_1(x)
-        print (list1)
-
 def x2(x):
-    x = x / 2
-    return collatz(x)
+    x=int(x / 2)
+    collatz(x)
 
 def x3_1(x):
     x=int(x*3+1)
